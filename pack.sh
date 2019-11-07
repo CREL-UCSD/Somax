b9c07d6e36a74d4b441f23d0034a7a89f64cca33
Kevin Huang <origamijr@sbcglobal.net>
2019-11-07 09:08:01 -0800

    trying again

---
+++ pack.sh
@@ -0,0 +1,2 @@
+gzip ./conductor_midi.app/Contents/Frameworks/Chromium\ Embedded\ Framework.framework/Chromium\ Embedded\ Framework
+gzip ./conductor_audio.app/Contents/Frameworks/Chromium\ Embedded\ Framework.framework/Chromium\ Embedded\ Framework
---
+++ setup.sh
@@ -0,0 +1,2 @@
+gunzip ./conductor_midi.app/Contents/Frameworks/Chromium\ Embedded\ Framework.framework/Chromium\ Embedded\ Framework.gz
+gunzip ./conductor_audio.app/Contents/Frameworks/Chromium\ Embedded\ Framework.framework/Chromium\ Embedded\ Framework.gz
